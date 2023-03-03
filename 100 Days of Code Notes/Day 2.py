cd3a7f5e9834c1dc769db020f85d7ca18f892818
DevonFrazer <109384563+DevonFrazer@users.noreply.github.com>
2023-03-02 19:18:31 -0500

    Create Day 2.py

---
+++ 100 Days of Code Notes/Day 2.py
@@ -0,0 +1,24 @@
+# print("Welcome to the Band Name Generator.")
+# street = input("What's the name of the city you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print("Your band name could be " + street + " " + pet)
+
+# two_digit_number = input("Type a two digit number: ")
+# first_digit = int(two_digit_number[0])
+# second_digit = int(two_digit_number[1])
+# two_digit_number = first_digit + second_digit
+# print(two_digit_number)
+# #End day 1 on Line 10
+
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# bmi= int(weight) / float(height) **2
+# bmi_as_integer= int(bmi)
+# print(bmi_as_integer)
+
+# age = input("What is your current age? ")
+# new_age = int(age)
+# days = int(90*365 - 365*new_age)
+# weeks = int(90*52 - 52*new_age)
+# months= int(90*12 - 12*new_age)
+# print(f"You have {days} days, {weeks} weeks, and {months} months left.")
