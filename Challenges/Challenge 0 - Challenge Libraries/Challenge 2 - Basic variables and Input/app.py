086377e7fa2f59c8750330ca13d0abc5954290c7
Kyle Martin <kyle.martin.0819@gmail.com>
2023-03-05 14:25:10 -0500

    Created challenge 2
    Created challenge two  app.py, readme.md files, and the created solution.

---
+++ Challenges/Challenge 0 - Challenge Libraries/Challenge 2 - Basic variables and Input/app.py
@@ -0,0 +1,14 @@
+#  Challenge Two: Basic Strings and Input
+# 
+#   The goal of this challege is to create two variables. One named name and one named greeting.
+#   Use the input method with the name variable to ask the user what their name is.
+#   Use the greeting variable to concatenate strings with the name variable to say:
+#   Hello, user.
+#   Use the print method to print the greeting to the terminal.
+
+def requestName():
+    # fill out challenge code here
+
+# main method -- do not worry about editing
+if __name__ == "__main__":
+    requestName()
