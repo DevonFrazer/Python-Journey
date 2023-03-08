6601cd366e3016845913fd3b94b578cd12a1b5a6
DevonFrazer <109384563+DevonFrazer@users.noreply.github.com>
2023-03-08 12:09:40 -0500

    Leap Year Calculator
    This calculator helps determine if a selected year is  a leap year or not utilizing if/else statements.
    Also was introduced to Modulo Operations for finding remainders.

--- 100 Days of Code/app.py
+++ 100 Days of Code/app.py
@@ -1,9 +1,12 @@
-print("Welcome to the tip calculator! ")
-bill = float(input("What is the amount for your bill? $"))
-numb_of_people = int(input("How many people would you like to split the bill with? "))
-tip_percentage = int(input("What percentage would you like to give? 10, 12, or 15? "))
-bill_with_tip = tip_percentage / 100 * bill + bill
-bill_per_person = bill_with_tip/numb_of_people
-final_amount = round(bill_per_person, 2)
-final_amount = "{:.2f}".format(bill_per_person)
-print(f"Each person should pay: ${final_amount}.")
+year= int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print ("Not leap year.")
