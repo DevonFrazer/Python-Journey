438b620d444cca49491768cb367812593b446a49
Kyle Martin <kyle.martin.0819@gmail.com>
2023-03-09 18:58:15 -0500

    Created Challenge 4 String Escape Characters
    Created a new challenge that will test if the user understands how to use one print method but print multiple lines to the terminal.

---
+++ Challenges/Challenge 0 - Challenge Libraries/Challenge 4 - Escape Characters/app.py
@@ -0,0 +1,10 @@
+# Code the rest of this method:
+def requestInformation():
+    # Place variables here
+    
+    # Add functionality here
+    
+
+# Main method - Do not edit
+if __name__ == "__main__":
+    requestInformation()
